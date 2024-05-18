0efa4cef6f94cfffb7839f5f9292b0591e679f30
konsti219 <37149441+konsti219@users.noreply.github.com>
2024-05-18 13:22:21 +0200

    Disable virtualbox due to build failure

--- nixos/virtualisation.nix
+++ nixos/virtualisation.nix
@@ -1,7 +1,7 @@
 # Options relating to Virtualisation
 {host, ...}: {
-  virtualisation.virtualbox.host.enable = true;
-  users.extraGroups.vboxusers.members = [host.mainUser];
+  # virtualisation.virtualbox.host.enable = true;
+  # users.extraGroups.vboxusers.members = [host.mainUser];
 
   # virtualisation.docker.enable = true;
   # users.extraGroups.docker.members = [ host.mainUser ];
