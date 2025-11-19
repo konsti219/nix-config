78116c22065906353425160361da25c605161ce1
konsti219 <37149441+konsti219@users.noreply.github.com>
2025-11-19 14:29:04 +0100

    add support for displaylink

--- nixos/general.nix
+++ nixos/general.nix
@@ -11,7 +11,7 @@
   # ====
 
   # Use latest Linux kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
   # For better interop
   boot.supportedFilesystems = ["ntfs"];
 
@@ -52,6 +52,7 @@
       "steam-run"
       "steam-unwrapped"
       "zerotierone"
+      "displaylink"
     ];
 
   nixpkgs.overlays = [
--- nixos/plasma.nix
+++ nixos/plasma.nix
@@ -1,28 +1,55 @@
-# Plasma 5 Configuration with XServer
-{pkgs, ...}: {
+# Plasma 6 Configuration with Wayland
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   # ===
   # KDE
   # ===
 
-  # Enable the Plasma 5 Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+  environment.variables = {
+    KWIN_DRM_PREFER_COLOR_DEPTH = "24";
+  };
+
+  # Enable the Plasma Desktop Environment.
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+    defaultSession = "plasma";
+  };
   services.desktopManager.plasma6.enable = true;
 
   # KDE Connect
   programs.kdeconnect.enable = true;
-  networking.firewall = {
-    allowedTCPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
-    allowedUDPPortRanges = [
-      {
-        from = 1714;
-        to = 1764;
-      }
-    ];
+  programs.kdeconnect.package = lib.mkForce pkgs.unstable.kdePackages.kdeconnect-kde;
+  # programs.kdeconnect.package = pkgs.plasma5Packages.kdeconnect-kde;
+
+  services.ratbagd.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    displaylink
+  ];
+  # services.xserver.videoDrivers = ["displaylink" "modesetting"];
+  boot = {
+    extraModulePackages = [config.boot.kernelPackages.evdi];
+    initrd.kernelModules = ["evdi"];
+  };
+  systemd.services.displaylink-server = {
+    enable = true;
+    requires = ["systemd-udevd.service"];
+    after = ["systemd-udevd.service"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.displaylink}/bin/DisplayLinkManager";
+      User = "root";
+      Group = "root";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
   };
 }
