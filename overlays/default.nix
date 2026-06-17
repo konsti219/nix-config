829a57e849d1083ded039ca5f3c4ba493ae094e2
konsti219 <37149441+konsti219@users.noreply.github.com>
2026-06-17 11:57:04 +0200

    use newest libratbad

--- overlays/default.nix
+++ overlays/default.nix
@@ -19,6 +19,18 @@
       system = final.stdenv.hostPlatform.system;
       config.allowUnfree = true;
       overlays = [
+        # Use newer libratbag
+        (ufinal: uprev: {
+          libratbag = uprev.libratbag.overrideAttrs (oldAttrs: {
+            version = "unstable-2026-05-31";
+            src = ufinal.fetchFromGitHub {
+              owner = "libratbag";
+              repo = "libratbag";
+              rev = "2fb9a701e8c02bbe261eb141ff311a379837c63d";
+              hash = "sha256-c4nAVhI3m9VeGy+rZLPS8Z98RS9JbrHe/mdiuee5y4s=";
+            };
+          });
+        })
         # Include wayvr overlay to have it in unstable
         (final: prev: {
           wayvr = inputs.wayvr.packages.${final.stdenv.hostPlatform.system}.default;
